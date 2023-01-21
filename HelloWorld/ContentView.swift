61468333cf037c8193113c72588b46a046bf15ca
zhaoxiangyu <51239614+liseami@users.noreply.github.com>
2023-01-21 14:28:17 +0800

    起飞版本
    改成了飞机

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "airplane.departure")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColor(.red)
+                .font(.largeTitle)
+            Text("起飞！")
         }
         .padding()
+        .scaleEffect(3)
     }
 }
 
---
+++ HelloWorld/SubView.swift
@@ -0,0 +1,20 @@
+//
+//  SubView.swift
+//  HelloWorld
+//
+//  Created by 赵翔宇 on 2023/1/21.
+//
+
+import SwiftUI
+
+struct SubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SubView_Previews: PreviewProvider {
+    static var previews: some View {
+        SubView()
+    }
+}
