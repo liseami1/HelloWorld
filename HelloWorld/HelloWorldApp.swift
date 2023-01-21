3edfb03f3770c5a45680d76f73e642bc1f13c6b5
zhaoxiangyu <51239614+liseami@users.noreply.github.com>
2023-01-21 14:24:41 +0800

    初次提交
    初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交初次提交

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 赵翔宇 on 2023/1/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
